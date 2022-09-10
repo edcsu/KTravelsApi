ac4481d5beb09aab223e2f7b9bbfff1904cfe91e
Sewannonda Keith Edwin <skeith696@gmail.com>
2022-09-10 19:35:52 +0300

    feat: add paginated response

---
+++ KTravelsApi/Core/ViewModels/PaginatedResponse.cs
@@ -0,0 +1,14 @@
+﻿namespace KTravelsApi.Core.ViewModels;
+
+public class PaginatedResponse<T>
+{
+    public PaginatedResponse(SearchPagination pagination, T items)
+    {
+        Pagination = pagination;
+        this.Items = items;
+    }
+
+    public SearchPagination Pagination { get; set; }
+ 
+    public T Items { get; set; }
+}
---
+++ KTravelsApi/Core/ViewModels/SearchPagination.cs
@@ -0,0 +1,32 @@
+﻿namespace KTravelsApi.Core.ViewModels;
+
+/// <summary>
+/// The details of pagination of the requests
+/// </summary>
+public class SearchPagination
+{
+    private const int MaxPageSize = 100;
+
+    /// <summary>
+    /// Page number
+    /// </summary>
+    public int Page { get; set; } = 1;
+
+    private int _itemsPerPage = 50;
+
+    /// <summary>
+    /// Number of requests returned per page
+    /// </summary>
+    public int ItemsPerPage
+    {
+        get => _itemsPerPage;
+
+        set => _itemsPerPage = value > MaxPageSize ? MaxPageSize : value;
+    }
+
+    /// <summary>
+    /// Total number of requests
+    /// </summary>
+    public int TotalItems { get; set; }
+
+}
